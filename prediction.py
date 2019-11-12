2489e56184236c83f1f145a3373006d8ef543691
Watanabe Naoki <asterisk37n@gmail.com>
2019-11-13 00:42:41 +0900

    Fix input scale

--- prediction.py
+++ prediction.py
@@ -15,8 +15,9 @@ import os
 
 def load_image(path, shape):
     img = imageio.imread(path)
-    resized_img = skimage.transform.resize(img, shape)
-    return resized_img
+    resized_img = skimage.transform.resize(img, shape, preserve_range=True)
+    rescaled_img = resized_img / 127.5 - 1
+    return rescaled_img
 
 def predict(lndmk_image_paths, style_frame_paths, style_lndmk_paths):
     frame_shape = (256, 256, 3)
@@ -24,16 +25,17 @@ def predict(lndmk_image_paths, style_frame_paths, style_lndmk_paths):
     batch_size = lndmk_images.shape[0]
     style_frame_images = np.array([load_image(path, frame_shape) for path in style_frame_paths])
     style_lndmk_images = np.array([load_image(path, frame_shape) for path in style_lndmk_paths])
+    style = np.concatenate((style_lndmk_images, style_frame_images), axis=-1)
 
     gan = GAN(input_shape=frame_shape, num_videos=1, k=1)
     gene = gan.build_generator()
     embe = gan.build_embedder()
 
-    embe.load_weights('trained_models/monalisa_fewshot_embedder_in_combined.h5')
+    embe.load_weights('trained_models/0_meta_embedder_in_combined.h5')
     gene.load_weights('trained_models/monalisa_fewshot_generator_in_combined.h5')
 
-    style_embedding = embe.predict([style_frame_images, style_lndmk_images])
-    style_embedding = style_embedding.repeat(8, axis=0)
+    style_embedding = embe.predict(style)
+    style_embedding = style_embedding.repeat(8, axis=0)  # [512] -> [8, 512]
     fake_images = gene.predict([lndmk_images, style_embedding])
     return fake_images
 
@@ -42,6 +44,8 @@ if __name__ == '__main__':
     lndmk_file_paths = ['datasets/voxceleb2-9f/train/lndmks/id00012/21Uxsk56VDQ/{}.jpg'.format(i) for i in range(8)]
     style_frame_paths = ['datasets/fewshot/monalisa/frames/monalisa256-0.jpg']
     style_lndmk_paths = ['datasets/fewshot/monalisa/lndmks/monalisa256-0.jpg']
+#    style_frame_paths = ['datasets/voxceleb2-9f/train/frames/id00012/21Uxsk56VDQ/{}.jpg'.format(i) for i in range(8)]
+#    style_lndmk_paths = ['datasets/voxceleb2-9f/train/lndmks/id00012/21Uxsk56VDQ/{}.jpg'.format(i) for i in range(8)]
     fake_images = predict(lndmk_file_paths, style_frame_paths, style_lndmk_paths)
     fake_images += 1
     fake_images *= 127.5
