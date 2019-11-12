3ccb755b72d13069d2910bde744f70599b80028b
Watanabe Naoki <asterisk37n@gmail.com>
2019-11-13 00:42:03 +0900

    Change naming convention and remove unnecessary code

--- fewshot_learning.py
+++ fewshot_learning.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 from keras.models import Model, load_model, model_from_json
 from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization
 from keras.optimizers import Adam
@@ -8,14 +11,10 @@ import tensorflow as tf
 from models import GAN
 from utils import AdaIN, ConvSN2D, SelfAttention, DenseSN, Bias, GlobalSumPooling2D, Bias
 from data_loader import flow_from_dir
-from meta_learning import perceptual_loss, hinge_loss
-
-import logging
-import os
 
 
 def fewshot_learn():
-    metalearning_epoch=2
+    metalearning_epoch=0
     BATCH_SIZE = 1
     k = 1
     frame_shape = h, w, c = (256, 256, 3)
@@ -36,53 +35,52 @@ def fewshot_learn():
         intermediate_vggface = gan.intermediate_vggface
         intermediate_discriminator = gan.intermediate_discriminator
 
-    logger.info('==== discriminator ===')
-    discriminator.summary(print_fn=logger.info)
-    logger.info('=== generator ===')
-    combined.get_layer('generator').summary(print_fn=logger.info)
-    logger.info('=== embedder ===')
-    combined.get_layer('embedder').summary(print_fn=logger.info)
-
     discriminator.load_weights('trained_models/{}_meta_discriminator_weights.h5'.format(metalearning_epoch), by_name=True, skip_mismatch=True)
     combined.get_layer('embedder').load_weights('trained_models/{}_meta_embedder_in_combined.h5'.format(metalearning_epoch))
     combined.get_layer('generator').load_weights('trained_models/{}_meta_generator_in_combined.h5'.format(metalearning_epoch))
     
     for epoch in range(epochs):
-        for batch_ix, (frames, landmarks, embedding_frames, embedding_landmarks) in enumerate(flow_from_dir(datapath, num_videos, (h, w), BATCH_SIZE, k, meta=False)):
+        for batch_ix, (frames, landmarks, styles) in enumerate(flow_from_dir(datapath, num_videos, (h, w), BATCH_SIZE, k, meta=False)):
             if batch_ix == num_batches:
                 break
             valid = np.ones((frames.shape[0], 1))
             invalid = - valid
 
-            intermediate_vgg19_outputs = intermediate_vgg19.predict_on_batch(frames)
-            intermediate_vggface_outputs =intermediate_vggface.predict_on_batch(frames)
-            intermediate_discriminator_outputs = intermediate_discriminator.predict_on_batch([frames, landmarks])
-            e_hat = embedder.predict_on_batch([embedding_frames, embedding_landmarks])
+            intermediate_vgg19_reals = intermediate_vgg19.predict_on_batch(frames)
+            intermediate_vggface_reals = intermediate_vggface.predict_on_batch(frames)
+            intermediate_discriminator_reals = intermediate_discriminator.predict_on_batch([frames, landmarks])
 
-            fake_frames = generator.predict_on_batch([landmarks, e_hat])
-            
+            style_list = [styles[:, i, :, :, :] for i in range(k)]
+
             g_loss = combined_to_train.train_on_batch(
-                [landmarks, embedding_frames, embedding_landmarks],
-                intermediate_vgg19_outputs + intermediate_vggface_outputs + [valid] + intermediate_discriminator_outputs
+                [landmarks] + style_list,
+                intermediate_vgg19_reals + intermediate_vggface_reals + [valid] + intermediate_discriminator_reals
             )
+
+            embeddings_list = [embedder.predict_on_batch(style) for style in style_list]
+            average_embedding = np.mean(np.array(embeddings_list), axis=0)
+
             d_loss_real = discriminator_to_train.train_on_batch(
-                [frames, landmarks, e_hat],
+                [frames, landmarks, average_embedding],
                 [valid]
             )
+            
+
+            fake_frames = generator.predict_on_batch([landmarks, average_embedding])
             d_loss_fake = discriminator_to_train.train_on_batch(
-                [fake_frames, landmarks, e_hat],
+                [fake_frames, landmarks, average_embedding],
                 [invalid]
             )
-        logger.info((epoch, batch_ix, g_loss, (d_loss_real, d_loss_fake)))
+            logger.info((epoch, batch_ix, g_loss, (d_loss_real, d_loss_fake)))
 
     # Save whole model
-    combined.save('trained_models/{}_fewshot_combined.h5'.format(dataname))
-    discriminator.save('trained_models/{}_fewshot_discriminator.h5'.format(dataname))
+#    combined.save('trained_models/{}_fewshot_combined.h5'.format(dataname))
+#    discriminator.save('trained_models/{}_fewshot_discriminator.h5'.format(dataname))
 
     # Save weights only
-    combined.save_weights('trained_models/{}_fewshot_combined_weights.h5'.format(dataname))
+#    combined.save_weights('trained_models/{}_fewshot_combined_weights.h5'.format(dataname))
     combined.get_layer('generator').save_weights('trained_models/{}_fewshot_generator_in_combined.h5'.format(dataname))
-    combined.get_layer('embedder').save_weights('trained_models/{}_fewshot_embedder_in_combined.h5'.format(dataname))
+#    combined.get_layer('embedder').save_weights('trained_models/{}_fewshot_embedder_in_combined.h5'.format(dataname))
     discriminator.save_weights('trained_models/{}_fewshot_discriminator_weights.h5'.format(dataname))
         
 
